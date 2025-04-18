2b931e18a8f4127af62ab3fda59c2ab6ed317bf1
Yiwei Gong <imwithye@gmail.com>
2025-04-18 10:53:42 +0800

    implement get apis

--- src/argocd/client.ts
+++ src/argocd/client.ts
@@ -1,4 +1,11 @@
-import { ApplicationList } from '../shared/models/models.js';
+import {
+  Application,
+  ApplicationList,
+  ApplicationTree,
+  EventList,
+  ResourceAction,
+  ResourceDiff
+} from '../shared/models/models.js';
 import { HttpClient } from './http.js';
 
 export class ArgoCDClient {
@@ -13,7 +20,40 @@ export class ArgoCDClient {
   }
 
   public async getApplications() {
-    const { body } = await this.client.get<ApplicationList>('/api/v1/applications');
+    const { body } = await this.client.get<ApplicationList>(`/api/v1/applications`);
+    return body;
+  }
+
+  public async getApplication(name: string) {
+    const { body } = await this.client.get<Application>(`/api/v1/applications/${name}`);
+    return body;
+  }
+
+  public async getApplicationResourceTree(name: string) {
+    const { body } = await this.client.get<ApplicationTree>(
+      `/api/v1/applications/${name}/resource-tree`
+    );
+    return body;
+  }
+
+  public async getApplicationManagedResources(name: string) {
+    const { body } = await this.client.get<{ items: ResourceDiff[] }>(
+      `/api/v1/applications/${name}/managed-resources`
+    );
+    return body;
+  }
+
+  public async getWorkloadLogs() {}
+
+  public async getResourceEvents(name: string) {
+    const { body } = await this.client.get<EventList>(`/api/v1/applications/${name}/events`);
+    return body;
+  }
+
+  public async getResourceActions(name: string) {
+    const { body } = await this.client.get<{ actions: ResourceAction[] }>(
+      `/api/v1/applications/${name}/resource/actions`
+    );
     return body;
   }
 }
--- src/server/server.ts
+++ src/server/server.ts
@@ -1,8 +1,9 @@
-import { McpServer } from '@modelcontextprotocol/sdk/server/mcp.js';
+import { McpServer, ToolCallback } from '@modelcontextprotocol/sdk/server/mcp.js';
 import { Implementation } from '@modelcontextprotocol/sdk/types.js';
 
 import packageJSON from '../../package.json' with { type: 'json' };
 import { ArgoCDClient } from '../argocd/client.js';
+import { z, ZodRawShape } from 'zod';
 
 export class Server extends McpServer {
   private argocdClient: ArgoCDClient;
@@ -14,11 +15,63 @@ export class Server extends McpServer {
       process.env.ARGOCD_API_TOKEN || ''
     );
 
-    this.tool('getApplications', 'Get all applications', {}, async () => {
-      const applications = await this.argocdClient.getApplications();
-      return {
-        content: [{ type: 'text', text: JSON.stringify(applications) }]
-      };
+    this.addJsonOutputTool(
+      'get_applications',
+      'get_applications returns list of applications',
+      {},
+      async () => await this.argocdClient.getApplications()
+    );
+    this.addJsonOutputTool(
+      'get_application',
+      'get_application returns application by application name',
+      { name: z.string() },
+      async ({ name }) => await this.argocdClient.getApplication(name)
+    );
+    this.addJsonOutputTool(
+      'get_application_resource_tree',
+      'get_application_resource_tree returns resource tree for application by application name',
+      { name: z.string() },
+      async ({ name }) => await this.argocdClient.getApplicationResourceTree(name)
+    );
+    this.addJsonOutputTool(
+      'get_application_managed_resources',
+      'get_application_managed_resources returns managed resources for application by application name',
+      { name: z.string() },
+      async ({ name }) => await this.argocdClient.getApplicationManagedResources(name)
+    );
+    this.addJsonOutputTool(
+      'get_application_resource_events',
+      'get_application_resource_events returns resource events for application by application name',
+      { name: z.string() },
+      async ({ name }) => await this.argocdClient.getResourceEvents(name)
+    );
+    this.addJsonOutputTool(
+      'get_application_resource_actions',
+      'get_application_resource_actions returns resource actions for application by application name',
+      { name: z.string() },
+      async ({ name }) => await this.argocdClient.getResourceActions(name)
+    );
+  }
+
+  private addJsonOutputTool<Args extends ZodRawShape, T>(
+    name: string,
+    description: string,
+    paramsSchema: Args,
+    cb: (...cbArgs: Parameters<ToolCallback<Args>>) => T
+  ) {
+    this.tool(name, description, paramsSchema as ZodRawShape, async (...args) => {
+      try {
+        const result = await cb.apply(this, args as Parameters<ToolCallback<Args>>);
+        return {
+          isError: false,
+          content: [{ type: 'text', text: JSON.stringify(result) }]
+        };
+      } catch (error) {
+        return {
+          isError: true,
+          content: [{ type: 'text', text: error instanceof Error ? error.message : String(error) }]
+        };
+      }
     });
   }
 }
