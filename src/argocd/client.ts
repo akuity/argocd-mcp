9ffcdcd31a5ba70aade294fe8ca3e99d60faea18
Yiwei Gong <imwithye@gmail.com>
2025-04-21 11:10:21 +0800

    add url params for post put delete

--- src/argocd/client.ts
+++ src/argocd/client.ts
@@ -33,6 +33,7 @@ export class ArgoCDClient {
   public async createApplication(application: Application) {
     const { body } = await this.client.post<Application, Application>(
       `/api/v1/applications`,
+      null,
       application
     );
     return body;
@@ -41,6 +42,7 @@ export class ArgoCDClient {
   public async updateApplication(applicationName: string, application: Application) {
     const { body } = await this.client.put<Application, Application>(
       `/api/v1/applications/${applicationName}`,
+      null,
       application
     );
     return body;
@@ -106,7 +108,13 @@ export class ArgoCDClient {
     resourceName: string
   ) {
     const { body } = await this.client.get<EventList>(
-      `/api/v1/applications/${applicationName}/events?appNamespace=${applicationNamespace}&resourceUID=${resourceUID}&resourceNamespace=${resourceNamespace}&resourceName=${resourceName}`
+      `/api/v1/applications/${applicationName}/events`,
+      {
+        appNamespace: applicationNamespace,
+        resourceNamespace,
+        resourceUID,
+        resourceName
+      }
     );
     return body;
   }
@@ -117,7 +125,15 @@ export class ArgoCDClient {
     resourceRef: ResourceRef
   ) {
     const { body } = await this.client.get<{ actions: ResourceAction[] }>(
-      `/api/v1/applications/${applicationName}/resource/actions?appNamespace=${applicationNamespace}&namespace=${resourceRef.namespace}&group=${resourceRef.group}&kind=${resourceRef.kind}&resourceName=${resourceRef.name}&version=`
+      `/api/v1/applications/${applicationName}/resource/actions`,
+      {
+        appNamespace: applicationNamespace,
+        namespace: resourceRef.namespace,
+        resourceName: resourceRef.name,
+        group: resourceRef.group,
+        kind: resourceRef.kind,
+        version: resourceRef.version
+      }
     );
     return body;
   }
@@ -129,7 +145,15 @@ export class ArgoCDClient {
     action: string
   ) {
     const { body } = await this.client.post<string, Application>(
-      `/api/v1/applications/${applicationName}/resource/actions?appNamespace=${applicationNamespace}&namespace=${resourceRef.namespace}&group=${resourceRef.group}&kind=${resourceRef.kind}&resourceName=${resourceRef.name}&version=`,
+      `/api/v1/applications/${applicationName}/resource/actions`,
+      {
+        appNamespace: applicationNamespace,
+        namespace: resourceRef.namespace,
+        resourceName: resourceRef.name,
+        group: resourceRef.group,
+        kind: resourceRef.kind,
+        version: resourceRef.version
+      },
       action
     );
     return body;
